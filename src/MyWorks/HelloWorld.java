0242730fe295ff699eac5942991ac06ceff26f82
meralyildiz <meral.yildiz@hotmail.com>
2022-08-09 14:10:22 +0200

    started hello feature

--- src/MyWorks/HelloWorld.java
+++ src/MyWorks/HelloWorld.java
@@ -2,7 +2,11 @@ package MyWorks;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello");
+        System.out.println("Developing hello feature");
+        System.out.println("Hello everyone");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
 
     }
 }
